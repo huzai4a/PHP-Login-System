1351561befb37a8011b63d824d0bd39845a4d0f7
huzai4a <huzaifa_syed78@hotmail.com>
2021-09-01 13:54:30 -0400

    added uikit and login form

--- index.php
+++ index.php
@@ -1,5 +1,51 @@
-<?php 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
+    </head>
+    <body>
 
-echo "hello world";
+    <script src="script.js"></script>
 
-?>
+
+    <div class="uk-section uk-container">
+    	<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid=" ">
+	    		<form class="uk-form-stacked js-login"> <!-- FORM START -->
+
+	    <div class="uk-margin">
+	        <label class="uk-form-label" for="form-stacked-text">Email</label>
+	        <div class="uk-form-controls">
+	            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+	        </div>
+	    </div>
+
+	    <div class="uk-margin">
+	        <label class="uk-form-label" for="form-stacked-text">Password</label>
+	        <div class="uk-form-controls">
+	            <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="your password">
+	        </div>
+	    <div class="uk-margin">
+	    	<button class="uk-button uk-button-default" type="submit"> Login </button>
+	        </div>
+	    </div>
+	</form> <!-- FORM END -->
+    	</div>
+    </div>
+    <?php
+
+
+//end of php/start of ui kit
+    ?>
+<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit-icons.min.js"></script>
+  </body>
+</html>
