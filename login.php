1ce131fe9bc2f93373f8b963dc0608e584d43774
huzai4a <huzaifa_syed78@hotmail.com>
2021-09-06 17:41:05 -0400

    copied head markup from original repo of instructor

--- login.php
+++ login.php
@@ -5,18 +5,20 @@
     require_once "inc/config.php";
     ?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Title</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/uikit.min.css" />
-        <script src="js/uikit.min.js"></script>
-        <script src="js/uikit-icons.min.js"></script>
-    </head>
-    <body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
 
-    <script src="script.js"></script>
+    <title>Page Title</title>
+
+    <base href="/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+  </head>
+
+  <body>
 
 
     <div class="uk-section uk-container">
--- register.php
+++ register.php
@@ -5,19 +5,20 @@
     require_once "inc/config.php";
     ?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Title</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/uikit.min.css" />
-        <script src="js/uikit.min.js"></script>
-        <script src="js/uikit-icons.min.js"></script>
-    </head>
-    <body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
 
-    <script src="script.js"></script>
+    <title>Page Title</title>
 
+    <base href="/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+  </head>
+
+  <body>
 
     <div class="uk-section uk-container">
     	<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
@@ -37,6 +38,10 @@
 	        <div class="uk-form-controls">
 	            <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="your password">
 	        </div>
+
+	        <div class="uk-margin uk-alert uk-alert-danger js-error" style="display: none;"></div>
+
+
 	    <div class="uk-margin">
 	    	<button class="uk-button uk-button-default" type="submit"> Register </button>
 	        </div>
