fb5e5fbb0b20f071f01cb811d21ffe4e8a2422dd
huzai4a <huzaifa_syed78@hotmail.com>
2021-09-06 17:39:35 -0400

    added jquery link

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,11 @@
+<!-- jQuery is required -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 <!-- UIkit CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/css/uikit.min.css" />
 
 <!-- UIkit JS -->
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit-icons.min.js"></script>
+
+<script src="/PHP-Login-System/assets/js/main.js"></script>
